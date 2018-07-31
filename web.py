8e0e14fe42aaed2d7269380c87737c721a7e8328
Kristian Glass <git@doismellburning.co.uk>
2018-07-31 09:56:13 +0000

    Stateful Klein setup
    http://klein.readthedocs.io/en/latest/examples/nonglobalstate.html

--- web.py
+++ web.py
@@ -1,17 +1,25 @@
 from klein import Klein
+import json
 
 
-app = Klein()
+class EinsteinWebServer(object):
 
+    app = Klein()
 
-@app.route('/')
-def hello(request):
-    return "Hello, world!"
+    def __init__(self, monitors=None):
+        self.monitors = monitors
+        if self.monitors is None:
+            self.monitors = {}
+
+    @app.route('/monitors')
+    def monitors(self, request):
+        request.setHeader('Content-Type', 'application/json')
+        return json.dumps(self.monitors)
 
 
 if __name__ == "__main__":
     from twisted.internet import reactor
     from twisted.web import server
 
-    reactor.listenTCP(8080, server.Site(app.resource()))
+    reactor.listenTCP(8080, server.Site(EinsteinWebServer().app.resource()))
     reactor.run()
