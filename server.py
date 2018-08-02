2ef5d0ac1382ea0070d9d28d1ac92c461b72a28f
Kristian Glass <git@doismellburning.co.uk>
2018-08-02 08:12:39 +0000

    Break out server definitions for more readability

--- server.py
+++ server.py
@@ -223,8 +223,10 @@ class IntellivueInterface(DatagramProtocol):
 if __name__ == '__main__':
     monitors = {}
     subscriptions = {}
-    reactor.listenTCP(8080, server.Site(web.EinsteinWebServer(monitors=monitors, subscriptions=subscriptions).app.resource()))
-    reactor.listenUDP(packets.PORT_CONNECTION_INDICATION, IntellivueInterface(monitors=monitors, subscriptions=subscriptions))
+    w = web.EinsteinWebServer(monitors=monitors, subscriptions=subscriptions).app.resource()
+    reactor.listenTCP(8080, server.Site(w))
+    i = IntellivueInterface(monitors=monitors, subscriptions=subscriptions)
+    reactor.listenUDP(packets.PORT_CONNECTION_INDICATION, i)
 
     print("Starting...")
     reactor.run()
