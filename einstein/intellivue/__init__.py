9816605861e8a504f8e07ada99381da31598b337
Kristian Glass <git@doismellburning.co.uk>
2018-08-14 14:06:40 +0000

    Pull out SPpdu, ROapdus, ROIVapdu, RORSapdu, ROLRSapdu and ROERapdu

--- einstein/intellivue/__init__.py
+++ einstein/intellivue/__init__.py
@@ -22,120 +22,6 @@ class Nomenclature(Packet):
     ]
 
 
-ROIV_APDU = 1
-RORS_APDU = 2
-ROER_APDU = 3
-ROLRS_APDU = 5
-
-def ROTypeField(name, default):
-    enum = {
-        ROIV_APDU: "ROIV_APDU",
-        RORS_APDU: "RORS_APDU",
-        ROER_APDU: "ROER_APDU",
-        ROLRS_APDU: "ROLRS_APDU",
-    }
-    return ShortEnumField(name, default, enum)
-
-
-class ROapdus(Packet):
-    name = "ROapdus"
-    fields_desc = [
-        ROTypeField("ro_type", 0),
-        LenField("length", None),
-    ]
-
-
-CMD_EVENT_REPORT = 0
-CMD_CONFIRMED_EVENT_REPORT = 1
-CMD_GET = 3
-CMD_SET = 4
-CMD_CONFIRMED_SET = 5
-CMD_CONFIRMED_ACTION = 7
-
-def CMDTypeField(name, default):  # PIPG-47
-    enum = {
-        CMD_EVENT_REPORT: "CMD_EVENT_REPORT",
-        CMD_CONFIRMED_EVENT_REPORT: "CMD_CONFIRMED_EVENT_REPORT",
-        CMD_GET: "CMD_GET",
-        CMD_SET: "CMD_SET",
-        CMD_CONFIRMED_SET: "CMD_CONFIRMED_SET",
-        CMD_CONFIRMED_ACTION: "CMD_CONFIRMED_ACTION",
-    }
-    return ShortEnumField(name, default, enum)
-
-
-class ROIVapdu(Packet):
-    name = "ROIVapdu"
-    fields_desc = [
-        ShortField("invoke_id", 0),
-        CMDTypeField("command_type", 0),
-        LenField("length", None),
-    ]
-
-
-class RORSapdu(Packet):  # PIPG-43
-    name = "RORSapdu"
-    fields_desc = [
-        ShortField("invoke_id", 0),
-        CMDTypeField("command_type", 0),
-        LenField("length", None),
-    ]
-
-
-class RorlsId(NonContainerPacket):  # PIPG-44
-    name = "RorlsId"
-    fields_desc = [
-        ByteField("state", 0),  # TODO Enum
-        ByteField("count", 0),
-    ]
-
-
-class ROLRSapdu(Packet):  # PIPG-44
-    name = "ROLRSapdu"
-    fields_desc = [
-        PacketField("linked_id", RorlsId(), RorlsId),
-        ShortField("invoke_id", 0),
-        CMDTypeField("command_type", 0),
-        LenField("length", None),
-    ]
-
-
-NO_SUCH_OBJECT_CLASS = 0
-NO_SUCH_OBJECT_INSTANCE = 1
-ACCESS_DENIED = 2
-GET_LIST_ERROR = 7
-SET_LIST_ERROR = 8
-NO_SUCH_ACTION = 9
-PROCESSING_FAILURE = 10
-INVALID_ARGUMENT_VALUE = 15
-INVALID_SCOPE = 16
-INVALID_OBJECT_INSTANCE = 17
-
-def ErrorValueField(name, default):  # PIPG-45
-    enum = {
-        NO_SUCH_OBJECT_CLASS: "NO_SUCH_OBJECT_CLASS",
-        NO_SUCH_OBJECT_INSTANCE: "NO_SUCH_OBJECT_INSTANCE",
-        ACCESS_DENIED: "ACCESS_DENIED",
-        GET_LIST_ERROR: "GET_LIST_ERROR",
-        SET_LIST_ERROR: "SET_LIST_ERROR",
-        NO_SUCH_ACTION: "NO_SUCH_ACTION",
-        PROCESSING_FAILURE: "PROCESSING_FAILURE",
-        INVALID_ARGUMENT_VALUE: "INVALID_ARGUMENT_VALUE",
-        INVALID_SCOPE: "INVALID_SCOPE",
-        INVALID_OBJECT_INSTANCE: "INVALID_OBJECT_INSTANCE",
-    }
-    return ShortEnumField(name, default, enum)
-
-
-class ROERapdu(NonContainerPacket):  # PIPG-45
-    name = "ROERapdu"
-    fields_desc = [
-        ShortField("invoke_id", 0),
-        ErrorValueField("error_value", 0),
-        LenField("length", None),
-    ]
-
-
 class EventReportArgument(Packet):
     name = "EventReportArgument"
     fields_desc = [
@@ -224,14 +110,6 @@ class IpAddressInfo(Packet):
 
 def ConnectIndication():
     return Nomenclature() / ROapdus() / ROIVapdu() / EventReportArgument() / AttributeList()
-
-
-class SPpdu(Packet):  # PIPG-42
-    name = "SPpdu"
-    fields_desc = [
-        ShortField("session_id", 0xE100), # "This field identifies a Protocol message. The field contains a fixed value 0xE100"
-        ShortField("context_id", 2), # If a Computer Client encodes the Association Control protocol commands as suggested in "Definition of the Association Control Protocol" on page 65, the context_id for the Data Export protocol commands is 2.
-    ]
 
 
 class MDSCreateInfo(NonContainerPacket):
--- einstein/intellivue/protocol_command_structure.py
+++ einstein/intellivue/protocol_command_structure.py
@@ -3,3 +3,102 @@ Protocol Command Structure - PIPG-41
 """
 
 from .common import *
+
+
+class SPpdu(Packet):  # PIPG-42
+    name = "SPpdu"
+    fields_desc = [
+        ShortField("session_id", 0xE100), # "This field identifies a Protocol message. The field contains a fixed value 0xE100"
+        ShortField("context_id", 2), # If a Computer Client encodes the Association Control protocol commands as suggested in "Definition of the Association Control Protocol" on page 65, the context_id for the Data Export protocol commands is 2.
+    ]
+
+
+def ROTypeField(name, default):
+    enum = {
+        ROIV_APDU: "ROIV_APDU",
+        RORS_APDU: "RORS_APDU",
+        ROER_APDU: "ROER_APDU",
+        ROLRS_APDU: "ROLRS_APDU",
+    }
+    return ShortEnumField(name, default, enum)
+
+
+class ROapdus(Packet):  # PIPG-42
+    name = "ROapdus"
+    fields_desc = [
+        ROTypeField("ro_type", 0),
+        LenField("length", None),
+    ]
+
+
+def CMDTypeField(name, default):  # PIPG-47
+    enum = {
+        CMD_EVENT_REPORT: "CMD_EVENT_REPORT",
+        CMD_CONFIRMED_EVENT_REPORT: "CMD_CONFIRMED_EVENT_REPORT",
+        CMD_GET: "CMD_GET",
+        CMD_SET: "CMD_SET",
+        CMD_CONFIRMED_SET: "CMD_CONFIRMED_SET",
+        CMD_CONFIRMED_ACTION: "CMD_CONFIRMED_ACTION",
+    }
+    return ShortEnumField(name, default, enum)
+
+
+class ROIVapdu(Packet):  # PIPG-43
+    name = "ROIVapdu"
+    fields_desc = [
+        ShortField("invoke_id", 0),
+        CMDTypeField("command_type", 0),
+        LenField("length", None),
+    ]
+
+
+class RORSapdu(Packet):  # PIPG-43
+    name = "RORSapdu"
+    fields_desc = [
+        ShortField("invoke_id", 0),
+        CMDTypeField("command_type", 0),
+        LenField("length", None),
+    ]
+
+
+class RorlsId(NonContainerPacket):  # PIPG-44
+    name = "RorlsId"
+    fields_desc = [
+        ByteField("state", 0),  # TODO Enum
+        ByteField("count", 0),
+    ]
+
+
+class ROLRSapdu(Packet):  # PIPG-44
+    name = "ROLRSapdu"
+    fields_desc = [
+        PacketField("linked_id", RorlsId(), RorlsId),
+        ShortField("invoke_id", 0),
+        CMDTypeField("command_type", 0),
+        LenField("length", None),
+    ]
+
+
+def ErrorValueField(name, default):  # PIPG-45
+    enum = {
+        NO_SUCH_OBJECT_CLASS: "NO_SUCH_OBJECT_CLASS",
+        NO_SUCH_OBJECT_INSTANCE: "NO_SUCH_OBJECT_INSTANCE",
+        ACCESS_DENIED: "ACCESS_DENIED",
+        GET_LIST_ERROR: "GET_LIST_ERROR",
+        SET_LIST_ERROR: "SET_LIST_ERROR",
+        NO_SUCH_ACTION: "NO_SUCH_ACTION",
+        PROCESSING_FAILURE: "PROCESSING_FAILURE",
+        INVALID_ARGUMENT_VALUE: "INVALID_ARGUMENT_VALUE",
+        INVALID_SCOPE: "INVALID_SCOPE",
+        INVALID_OBJECT_INSTANCE: "INVALID_OBJECT_INSTANCE",
+    }
+    return ShortEnumField(name, default, enum)
+
+
+class ROERapdu(NonContainerPacket):  # PIPG-45
+    name = "ROERapdu"
+    fields_desc = [
+        ShortField("invoke_id", 0),
+        ErrorValueField("error_value", 0),
+        LenField("length", None),
+    ]
